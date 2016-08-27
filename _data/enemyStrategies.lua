46f5baa5d6fe36e972e249de32c1e5bc75a9adad
xskaterboy118x <chrisbrady118@gmail.com>
2016-08-27 20:34:33 +0100

    fixes

--- _data/enemyStrategies.lua
+++ _data/enemyStrategies.lua
@@ -1,12 +1,13 @@
+
 -- One of these strategies will be picked by the enemy, they will loop through all of the actions in their strategy
 strategies = {
 	{
-		{"Basic Strike"},{"Basic Strike"},{"Basic Strike"},{"Basic Strike"},{"Leap Attack"}
+		{"Basic Strike","Basic Strike","Basic Strike","Basic Strike","Leap Attack","Basic Strike","CHARGE"}
 	},
 	{
-		{"GroundPound"},
+		{"Ground Pound","Ground Pound","Ground Pound","Rage","Ground Pound","Ground Pound","Ground Pound","Rage","CHARGE"}
 	},
 	{
-		{"Basic Strike"},
+		{"Dash","Dash","Basic Strike","Basic Strike","Basic Strike","Dash","Dash","Dash","Dash","CHARGE"}
 	}
 }
